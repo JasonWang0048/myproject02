d8a9af93df4c162c46ea7e65ee9ae260ca734fde
Jason <user@es301n29>
2019-02-26 12:04:11 +0800

    Collection

---
+++ myproject/src/com/quiz/Ticket.java
@@ -0,0 +1,12 @@
+package com.quiz;
+
+public class Ticket {
+	protected int num;
+	private String combo;
+	
+	public Ticket(int num, String combo) {
+		this.num = num;
+		this.combo = combo;
+	}
+	
+}
---
+++ myproject/src/com/tom/GraduateStudent.java
@@ -0,0 +1,8 @@
+package com.tom;
+
+public class GraduateStudent extends Student{
+	public GraduateStudent() {
+		super("Nobody", 0);
+		
+	}
+}
