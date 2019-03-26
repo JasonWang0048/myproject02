1f700dfda95e76c6bb038b3d6d0c298e8d6b7aa0
Jason <user@es301n29>
2019-03-26 12:17:20 +0800

    ShoppingList

--- myproject/src/com/quiz/ShoppingList.java
+++ myproject/src/com/quiz/ShoppingList.java
@@ -12,22 +12,25 @@ public class ShoppingList {
 		System.out.println("輸入0完成清單");
 		Scanner scan = new Scanner(System.in);
 		int x = scan.nextInt();
-		while(x != 0) {
 		List<String> list = new ArrayList<>();
+		while(x != 0) {
+//		List<String> list = new ArrayList<>();
 		if(x == 1) {
 			System.out.print("請輸入新增項目與數量: (項目/數量)");
 			String s = scan.nextLine();
+			list.add(s);
 			if(s.equals("0")) {
 				break;
 			}
-			list.add(s);
+			else if(s.equals("2")) {
+				System.out.println(list);
+				break;
+			}
+//			list.add(s);
 		}
-		else if(x == 2) {
-			System.out.println(list);
-		}
-		else {
-			break;
-		}
+//		else {
+//			break;
+//		}
 		}
 	}
 
